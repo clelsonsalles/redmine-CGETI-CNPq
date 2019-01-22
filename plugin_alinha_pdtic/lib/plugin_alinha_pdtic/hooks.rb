ef1812bc2dc57447c06ac7a60709b7817cf0eb83
root <clelson.salles@gmail.com>
2019-01-22 20:00:44 +0000

    Reconfiguracao do projeto de plugins

--- plugin_alinha_pdtic/lib/plugin_alinha_pdtic/hooks.rb
+++ plugin_alinha_pdtic/lib/plugin_alinha_pdtic/hooks.rb
@@ -1,12 +1,7 @@
 module PluginAlinhaPdtic
  class Hooks < Redmine::Hook::ViewListener
    referencias = Referencial.where(principal: nil)
-<<<<<<< HEAD
    alinhamentos_projeto = Alinhamento.where(project: @project)
    render_on :view_projects_show_right, :partial => 'hooks/view_projeto_alinhamento_pdtic', locals: { :referencias => referencias, :alinhamentos_projeto =>  alinhamentos_projeto }
-=======
-   alinhametos_projeto = Alinhamento.where(project: @project)
-   render_on :view_projects_show_right, :partial => 'hooks/view_projeto_alinhamento_pdtic', locals: { :referencias => referencias }
->>>>>>> 8be061619f1fd33593e18ecfdcb3c8e22b81f2c6
  end
 end
