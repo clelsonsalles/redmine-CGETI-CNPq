b0e799065516357c9aff4f4958c243f59eed4133
root <clelson.salles@gmail.com>
2019-01-28 19:01:27 +0000

    Atualiza visao de projetos da CGETI. Atualiza permissoes de acesso para as views de alinhamento e referencial

--- plugin_alinha_pdtic/lib/plugin_alinha_pdtic/hooks.rb
+++ plugin_alinha_pdtic/lib/plugin_alinha_pdtic/hooks.rb
@@ -3,5 +3,7 @@ module PluginAlinhaPdtic
    referencias = Referencial.where(principal: nil)
    alinhamentos_projeto = Alinhamento.where(project: @project)
    render_on :view_projects_show_right, :partial => 'hooks/view_projeto_alinhamento_pdtic', locals: { :referencias => referencias, :alinhamentos_projeto =>  alinhamentos_projeto }
+
+   render_on :view_projects_show_left, :partial => 'hooks/view_projeto_lista_arvore'
  end
 end
