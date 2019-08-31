2f9631efabe67dfe4ebc40ddcafbda3034b74dd2
Clelson Salles Rodrigues <clelson.salles@gmail.com>
2019-08-31 19:44:02 -0300

    Retirando hooks do PDTIC
    Para o módulo de gerenciamento de contratos não deve manter as alterações do PDTIC, realizadas por hooks

--- plugin_alinha_pdtic/lib/plugin_alinha_pdtic/hooks.rb
+++ plugin_alinha_pdtic/lib/plugin_alinha_pdtic/hooks.rb
@@ -2,8 +2,6 @@ module PluginAlinhaPdtic
  class Hooks < Redmine::Hook::ViewListener
    referencias = Referencial.where(principal: nil)
    alinhamentos_projeto = Alinhamento.where(project: @project)
-   render_on :view_projects_show_right, :partial => 'hooks/view_projeto_alinhamento_pdtic', locals: { :referencias => referencias, :alinhamentos_projeto =>  alinhamentos_projeto }
 
-   render_on :view_projects_show_left, :partial => 'hooks/view_projeto_lista_arvore'
  end
 end
